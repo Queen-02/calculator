250730712fd3ea01ec564a12ef69f902e9ed449c
Queen-02 <queenzyblack176@gmail.com>
2022-05-18 17:13:02 +0300

    added calculation codes to main activity file

--- app/src/main/java/com/queen/calculator/MainActivity.kt
+++ app/src/main/java/com/queen/calculator/MainActivity.kt
@@ -2,10 +2,82 @@ package com.queen.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    //initializing edit texts
+    lateinit var etNumberOne: EditText
+    lateinit var etNumberTwo: EditText
+
+    //initializing buttons
+    lateinit var btnAdd: Button
+    lateinit var btnSub: Button
+    lateinit var btnMode: Button
+    lateinit var btnMulti: Button
+
+    //initializing texviews
+    lateinit var tvResults : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //casting the edit texts
+        etNumberOne = findViewById(R.id.etNumberOne)
+        etNumberTwo = findViewById(R.id.etNumber2)
+
+        //casting the buttons
+        btnAdd = findViewById(R.id.btnAdd)
+        btnSub = findViewById(R.id.btnSub)
+        btnMode = findViewById(R.id.btnModulus)
+        btnMulti = findViewById(R.id.btnMultiply)
+
+        //casting the text view
+        tvResults = findViewById(R.id.tvResult)
+
+
+        //setting click listeners for each button
+        btnAdd.setOnClickListener {
+            val num1 = etNumberOne.toString()
+            val num2 = etNumberTwo.toString()
+
+            if (num1.isBlank()){
+                etNumberOne.error = "First number required"
+                return@setOnClickListener
+            }
+            if (num2.isBlank()){
+                etNumberTwo.error = "Second number required"
+                return@setOnClickListener
+            }
+
+            addValues(num1.toInt(), num2.toInt())
+            subtractValues(num1.toInt(), num2.toInt())
+            modulusValues(num1.toInt(), num2.toInt())
+            multipyValues(num1.toInt(), num2.toInt())
+        }
+    }
+
+    fun addValues(num1: Int, num2: Int){
+        val add = num1 + num2
+        tvResults.text = add.toString()
+
+    }
+
+    fun subtractValues(num1: Int, num2: Int){
+        val add = num1 - num2
+        tvResults.text = add.toString()
+    }
+
+    fun modulusValues(num1: Int, num2: Int){
+        val add = num1 % num2
+        tvResults.text = add.toString()
+    }
+
+    fun multipyValues(num1: Int, num2: Int){
+        val add = num1 * num2
+        tvResults.text = add.toString()
+
     }
 }
